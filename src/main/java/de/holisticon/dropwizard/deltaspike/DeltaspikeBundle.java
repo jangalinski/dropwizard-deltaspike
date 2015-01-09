efb1ea05d3e56d73918b70d82a571c7e9d854baf
Jan Galinski <jan.galinski@holisticon.de>
2015-01-09 01:07:30 +0100

    fix logging

--- src/main/java/de/holisticon/dropwizard/deltaspike/DeltaspikeBundle.java
+++ src/main/java/de/holisticon/dropwizard/deltaspike/DeltaspikeBundle.java
@@ -19,7 +19,7 @@ import javax.enterprise.inject.spi.CDI;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
-public class DeltaspikeManagedBundle<T extends Configuration> implements ConfiguredBundle<T>, Managed  {
+public class DeltaspikeBundle<T extends Configuration> implements ConfiguredBundle<T> {
 
     private final CdiContainer cdiContainer = CdiContainerLoader.getCdiContainer();
     private final Logger logger = getLogger(this.getClass());
@@ -27,7 +27,7 @@ public class DeltaspikeManagedBundle<T extends Configuration> implements Configu
 
     @Override
     public void initialize(Bootstrap<?> bootstrap) {
-        logger.error("bundle-init-1");
+        logger.error("------------------- bundle-init-1");
         cdiContainer.boot();
 
         cdiContainer.getContextControl().startContext(RequestScoped.class);
@@ -35,33 +35,35 @@ public class DeltaspikeManagedBundle<T extends Configuration> implements Configu
         cdiContainer.getContextControl().startContext(ApplicationScoped.class);
 
         BeanProvider.injectFields(bootstrap.getApplication());
-        logger.error("bundle-init-2");
+        logger.error("------------------- bundle-init-2");
     }
 
     @Override
     public void run(T configuration, Environment environment) throws Exception {
         logger.error("------------------- bundle-run-1");
-        environment.lifecycle().manage(this);
+        environment.lifecycle().manage(new Managed() {
+            @Override
+            public void start() throws Exception {
+                logger.error("------------------- managed-start-1");
+            }
+
+            @Override
+            public void stop() throws Exception {
+                logger.error("------------------- managed-stop-1");
+
+                try {
+                    cdiContainer.getContextControl().stopContexts();
+                    cdiContainer.shutdown();
+                    logger.info("shut down container");
+                } catch (final NullPointerException e) {
+                    // weld lifecycle already ended
+                    logger.debug("weld has been already shut down", e);
+                }
+                logger.error("------------------- managed-stop-2");
+            }
+        });
         logger.error("------------------- bundle-run-2");
     }
 
-    @Override
-    public void start() throws Exception {
-        logger.error("------------------- bundle-start-empty");
-    }
 
-    @Override
-    public void stop() throws Exception {
-        logger.error("------------------- bundle-stop-1");
-
-        try {
-            cdiContainer.getContextControl().stopContexts();
-            cdiContainer.shutdown();
-            logger.info("shut down container");
-        } catch (final NullPointerException e) {
-            // weld lifecycle already ended
-            logger.debug("weld has been already shut down", e);
-        }
-        logger.error("------------------- bundle-stop-2");
-    }
 }
--- src/test/java/de/holisticon/dropwizard/deltaspike/example/DeltaspikeExampleApplication.java
+++ src/test/java/de/holisticon/dropwizard/deltaspike/example/DeltaspikeExampleApplication.java
@@ -1,16 +1,19 @@
 package de.holisticon.dropwizard.deltaspike.example;
 
 import com.google.common.io.Resources;
-import de.holisticon.dropwizard.deltaspike.DeltaspikeManagedBundle;
+import de.holisticon.dropwizard.deltaspike.DeltaspikeBundle;
 import io.dropwizard.Application;
 import io.dropwizard.Configuration;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.slf4j.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
 import java.io.File;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 @ApplicationScoped
 public class DeltaspikeExampleApplication extends Application<DeltaspikeExampleApplication.Config> {
@@ -31,17 +34,24 @@ public class DeltaspikeExampleApplication extends Application<DeltaspikeExampleA
     @Inject
     private DummyResource dummyResource;
 
+    private final Logger logger = getLogger(this.getClass());
 
 
     @Override
     public void run(final Config config, final Environment environment) throws Exception {
+        logger.error("------------------- application-run-1");
+
         environment.healthChecks().register("dummy", dummyHealthCheck);
         environment.jersey().register(dummyResource);
+
+        logger.error("------------------- application-run-2               ----");
     }
 
     @Override
     public void initialize(Bootstrap<Config> bootstrap) {
-        bootstrap.addBundle(new DeltaspikeManagedBundle());
+        logger.error("------------------- application-init-1");
+        bootstrap.addBundle(new DeltaspikeBundle());
+        logger.error("------------------- application-init-2");
     }
 
     public static class Config extends Configuration {
